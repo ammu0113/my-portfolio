f3bb7bd412a39ec956595c149c1852b0fb92b1f8
Amulya Bandla <amulya.bandla01@gmail.com>
2025-02-20 09:57:25 -0500

    Enhanced search engine optimization for better visibility

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Experience from "./components/Experience/Experience";
 import Education from "./components/Education/Education";
 import Contact from "./components/Contact/Contact";
 import "./styles/main.scss";
+import SEO from "./components/SEO/SEO";
 
 // Import all SCSS files
 import "./components/Header/Header.scss";
@@ -23,6 +24,11 @@ import Footer from "./components/Footer/Footer";
 const App = () => {
   return (
     <PortfolioProvider>
+      <SEO
+        title="Amulya Bandla | Senior Full Stack Developer"
+        description="Senior Full Stack Developer with expertise in React.js and Node.js"
+        keywords="React, Node.js, Full Stack, Developer"
+      />
       <div className="app">
         <Header />
         <main className="main-content">
---
+++ src/components/SEO/SEO.jsx
@@ -0,0 +1,50 @@
+import React from 'react';
+import { Helmet } from 'react-helmet-async';
+import PropTypes from 'prop-types';
+
+const SEO = ({ 
+  title, 
+  description, 
+  keywords, 
+  image, 
+  url, 
+  type = 'website' 
+}) => {
+  const siteUrl = 'https://ammu0113.github.io/my-portfolio';
+  const imageUrl = image ? `${siteUrl}${image}` : `${siteUrl}/og-image.png`;
+  const canonicalUrl = url ? `${siteUrl}${url}` : siteUrl;
+
+  return (
+    <Helmet>
+      {/* Basic Meta Tags */}
+      <title>{title}</title>
+      <meta name="description" content={description} />
+      <meta name="keywords" content={keywords} />
+      <link rel="canonical" href={canonicalUrl} />
+
+      {/* Open Graph Tags */}
+      <meta property="og:url" content={canonicalUrl} />
+      <meta property="og:type" content={type} />
+      <meta property="og:title" content={title} />
+      <meta property="og:description" content={description} />
+      <meta property="og:image" content={imageUrl} />
+
+      {/* Twitter Card Tags */}
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:title" content={title} />
+      <meta name="twitter:description" content={description} />
+      <meta name="twitter:image" content={imageUrl} />
+    </Helmet>
+  );
+};
+
+SEO.propTypes = {
+  title: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
+  keywords: PropTypes.string,
+  image: PropTypes.string,
+  url: PropTypes.string,
+  type: PropTypes.string
+};
+
+export default SEO; 
